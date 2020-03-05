313f19b4a56bb01fa139f672fc430cda78d65568
danilo de souza novaes <danilo.novaes@hotmail.com.br>
2020-03-05 18:48:37 -0300

    Colocando o nome da empresa

---
+++ MonitorarPing/Monitorar.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorarPing
+{
+    class Monitorar
+    {
+
+        public void VerificarConexao( string ipp) {
+            string ip = ipp;
+            Boolean veriricar = false;
+            StringBuilder str = new StringBuilder();
+            Console.WriteLine("MONITORANDO O IP " + ip);
+
+            while (ip != "")
+            {
+                DateTime thisDay = DateTime.Now;
+                Ping p = new Ping();
+                PingReply pr = p.Send(ip);
+
+
+
+                str.AppendLine(string.Format("Address: {0}", pr.Address));
+                str.AppendLine(string.Format("Status: {0}", pr.Status));
+
+                //  Console.WriteLine(str.ToString());
+
+                if (pr.Status == IPStatus.TimedOut || pr.Status == IPStatus.DestinationHostUnreachable)
+                {
+                    EscreveArquivo("CONEXÃO PERDIDA DO IP " + ip + "  " + pr.Status.ToString() + " AS " + thisDay.Hour.ToString() + " " + thisDay.ToString() + thisDay.Hour.ToString() + thisDay.Minute.ToString() + thisDay.Second.ToString() + thisDay.Millisecond.ToString());
+                    Console.WriteLine("CONEXÃO PERDIDA DO IP " + ip + "  " + pr.Status.ToString() + " AS " + thisDay.Hour.ToString() + " " + thisDay.ToString() + thisDay.Hour.ToString() + thisDay.Minute.ToString() + thisDay.Second.ToString() + thisDay.Millisecond.ToString());
+                    veriricar = true;
+                }
+                else if (veriricar == true)
+                {
+
+                    Console.WriteLine("MONITORANDO O IP " + ip);
+                    veriricar = false;
+
+                }
+
+            }
+        
+        }
+
+
+
+        private void EscreveArquivo(string mensagem)
+        {
+
+
+            if (mensagem != "")
+            {
+                StreamWriter arquivo1;
+
+                //Colocando o endereço físico (caminho do arquivo texto) 
+                string Caminho = System.AppDomain.CurrentDomain.BaseDirectory.ToString() +@"\log\log.txt";
+
+                //usando o metodo e abrindo o arquivo texto
+                arquivo1 = File.AppendText(Caminho);
+
+                //continuando escrevendo neste arquivo
+                //escrevendo a partir da ultima linha 
+                arquivo1.WriteLine();
+                arquivo1.WriteLine(mensagem);
+
+
+                arquivo1.Close();
+
+            }
+        }
+
+
+
+    }
+}
---
+++ MonitorarPing/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.IO;
+
+namespace MonitorarPing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("MONITORAMENTO IP.", Console.Title);
+            Console.Write("==| ESTE PROGRAMA VAI MONITORAR O IP INFORMADO PARA VERIFICAR O MOMENTO QUE PERDER A CONEXÃO |==============  ");
+            Console.WriteLine("");
+            Console.WriteLine("ESCRITO POR DANILO NOVAES. ");
+            Console.WriteLine("VERSÃO 1.0");
+            Console.WriteLine("DW NEW SOFTWARE");
+            Console.WriteLine("");
+
+
+            string ip;
+            Monitorar monitorar = new Monitorar();
+            Console.Write("Digite O IP PARA MONITORAR ");
+            ip = Console.ReadLine();
+            monitorar.VerificarConexao(ip);
+                     
+            
+        }
+
+
+       
+
+
+        
+
+
+    }
+}
